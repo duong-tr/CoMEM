628726730911047a86c12290d37bf6ed20d1a61e
Duong Tran <d@duongtr.com>
2026-07-31 09:33:40 +0200

    ...

--- CoMEM-inference/ReasonVQA/run_reasonvqa_finetunekv_clip.py
+++ CoMEM-inference/ReasonVQA/run_reasonvqa_finetunekv_clip.py
@@ -152,7 +152,10 @@ if __name__ == "__main__":
 
     import pandas as pd
     batch_data = pd.read_json(data_path, lines=True).to_dict(orient='records')
-    batch_data['data_id'] = batch_data['question_id']
+    # rename column 'question_id' to 'data_id'
+    for item in batch_data:
+        item['data_id'] = item.pop('question_id')
+        del item['question_id']
     lang_batch_data = {lang_item['data_id']: lang_item for lang_item in batch_data}
 
     # double check data exists:
